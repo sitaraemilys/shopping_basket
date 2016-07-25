7165dbd0fa51c0d19225cfffc41add6d44cc747d
sitypop <sityemilyshah@gmail.com>
2016-07-25 09:43:56 +0100

    Second feature spec: basket has an empty order log

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -4,16 +4,21 @@ describe 'Feature tests' do
 
   describe '#initialize' do
 
+    socks = Product.new("Socks", "S01", 7.95)
+    jeans = Product.new("Jeans", "J01", 32.95)
+    blouse = Product.new("Blouse", "B01", 24.95)
+    products = Products.new
+    products.add(socks)
+    products.add(blouse)
+    products.add(jeans)
+    basket = Basket.new(products)
+
     it 'basket has a product catalog of products' do
-      socks = Product.new("Socks", "S01", 7.95)
-      jeans = Product.new("Jeans", "J01", 32.95)
-      blouse = Product.new("Blouse", "B01", 24.95)
-      products = Products.new
-      products.add(socks)
-      products.add(blouse)
-      products.add(jeans)
-      basket = Basket.new(products)
       expect(basket.product_catalog).to eq products.catalog
+    end
+
+    it 'basket has an empty order log' do
+      expect(basket.order_log).to be_empty
     end
     
   end
