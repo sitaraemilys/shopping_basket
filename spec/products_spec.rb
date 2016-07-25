27d64e04427eafa478dfa24bd046026fccd21ad2
sitypop <sityemilyshah@gmail.com>
2016-07-25 09:32:09 +0100

    Refactors

--- spec/products_spec.rb
+++ spec/products_spec.rb
@@ -2,17 +2,19 @@ require 'products'
 
 describe Products do
 
+  subject(:products) { described_class.new }
+  let(:socks) { double(:product) }
+
   describe '#initialize' do
     it 'has an empty catalog' do
-      expect(subject.catalog).to be_empty
+      expect(products.catalog).to be_empty
     end
   end
 
   describe '#add' do
     it 'adds a product to the catalog' do
-      socks = Product.new("Socks", "S01", 7.95)
-      subject.add(socks)
-      expect(subject.catalog).to eq [socks]
+      products.add(socks)
+      expect(products.catalog).to eq [socks]
     end
   end
 
