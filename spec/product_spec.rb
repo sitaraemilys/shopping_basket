0da3ea48e0becfaa24c2daf3b8b164c5be246839
sitypop <sityemilyshah@gmail.com>
2016-07-25 09:22:53 +0100

    Refactors

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -2,15 +2,15 @@ require 'product'
 
 describe Product do
 
+  subject(:socks) { described_class.new("Socks", "S01") }
+
   describe '#initialize' do
 
     it 'has a name' do
-      socks = Product.new("Socks", "S01")
       expect(socks.name).to eq "Socks"
     end
 
     it 'has a code' do
-      socks = Product.new("Socks", "S01")
       expect(socks.code).to eq "S01"
     end
   end
